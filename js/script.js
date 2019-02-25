3e382c6ade7b24ad09ce7d074faaa01d409a2801
Your Name <you@example.com>
2019-02-25 16:36:54 +0400

    Add collection cards in discover page

--- js/script.js
+++ js/script.js
@@ -1,19 +1,4 @@
 $(document).ready(function () {
     console.log("ready!");
-
-    $(".subnavbar li a").click(function() {
-        $(this).parent().addClass('selected').siblings().removeClass('selected');
-        console.log("salam");
-    });
-
-    var num = 50;
-
-    $(window).bind('scroll', function () {
-        if ($(window).scrollTop() > num) {
-            $('.menu-home').addClass('fixed');
-        } else {
-            $('.menu-home').removeClass('fixed');
-        }
-    });
-
+    
 });
