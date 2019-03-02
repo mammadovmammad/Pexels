6fa0ebdd2f36eb846249d32ef54dad1050a6cac1
Your Name <you@example.com>
2019-03-02 17:49:31 +0400

    Fixed navbar position fixed problem and some changes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
-$(document).ready(function () {
-    console.log("ready!");
-    
-});
+    $(document).ready(function(){
+        $(window).on("scroll",function(){
+          var wn = $(window).scrollTop();
+        if(wn > 120){
+            $(".home").css("background","#232a34");
+        }
+        else{
+            $(".home").css("background","transparent");
+        }
+      });
+    });
